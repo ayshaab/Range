d1e4610c81dba9986df0a02519b878d1422c0c54
Aysha Allard Brown <ayshaab@student.ubc.ca>
2020-11-18 20:52:31 -0800

    commit 5 files

--- R/range.R
+++ R/range.R
@@ -1,3 +1,14 @@
+#' Find the range of a vector
+#'
+#' @param x Numeric vector
+#'
+#' @return
+#' A vector containing the resulting value when the min is subtracted from the max.
+#'
+#' @export
+#'
+#' @examples
+#' range(1:10)
 range <- function(x) {
   max(x) - min(x)
 }
