730d63cf0159be6c79bddcaf460b38a4d1dbbde0
Aysha Allard Brown <ayshaab@student.ubc.ca>
2020-12-10 19:48:34 -0800

    Made change to test-range.R file

--- tests/testthat/test-range.R
+++ tests/testthat/test-range.R
@@ -1,3 +1,4 @@
-test_that("range function works", {
+#Test to confirm whether the range function works
+test_that("range function computes the max value minus the min value of a vector", {
   expect_equal(range(1:10), 10 - 1)
 })
